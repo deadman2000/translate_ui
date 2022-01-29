2016560d89f0378a0a1120da11d3d123d4e47651
Иван Марчуков <deadman2000@mail.ru>
2022-01-29 18:15:40 +0300

    fixed dots in clipboard

--- src/components/project/MonoText.jsx
+++ src/components/project/MonoText.jsx
@@ -2,15 +2,8 @@ import React from "react";
 import './MonoText.scss'
 
 export default function MonoText({text}: {text: string}) {
-    function copyHandler() {
-        const txt = window.getSelection().toString()
-        navigator.clipboard.writeText(txt.replaceAll('·', ' '))
-            .then()
-    }
-
-    const html = text.replaceAll(' ', '·').replaceAll(/(·+)/g, '<span>$1</span>')
+    const html = text.replaceAll(/( )/g, '<span> </span>')
     return <pre dangerouslySetInnerHTML={{__html: html}}
                 className="mono-text"
-                onCopy={copyHandler}
     />
 }
