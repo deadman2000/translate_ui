65a49e799ee249d83544ccd5c77503da3d12c3bf
Иван Марчуков <marchukov.ivan@gmail.com>
2023-01-04 19:23:12 +0300

    multiple files upload

--- src/pages/projects/patches/PatchUploadPage.jsx
+++ src/pages/projects/patches/PatchUploadPage.jsx
@@ -1,42 +1,87 @@
 import api from "@/api/Api";
 import {GlobalStore} from "@/stores/GlobalStore";
 import type {RouteProps} from "@/types/RouteProps";
-import {Button, FileInput, FormGroup, Intent, ProgressBar} from "@blueprintjs/core";
+import {Button, Intent, ProgressBar} from "@blueprintjs/core";
 import {IconNames} from "@blueprintjs/icons";
 import {inject} from "mobx-react";
 import React from "react";
-import {Container} from "react-bootstrap";
+import {Container, Table} from "react-bootstrap";
 import {withRouter} from "react-router-dom";
+import { FileUploader } from "react-drag-drop-files";
+
+type UploadFile = {
+    file: File,
+    progress: number,
+    uploaded: number,
+}
+
+type State = {
+    uploading: boolean,
+    progress: number,
+    totalSize: number,
+    uploadedSize: number,
+    files: UploadFile[]
+}
 
 @inject("global")
 @withRouter
-export default class PatchUploadPage extends React.Component<{global?: GlobalStore} & RouteProps> {
-    state = {
+export default class PatchUploadPage extends React.Component<{global?: GlobalStore} & RouteProps, State> {
+    state: State = {
         uploading: false,
         progress: 0,
-        file: null,
+        totalSize: 0,
+        files: [],
     }
 
     render() {
-        const {uploading, file} = this.state
+        const {uploading, files} = this.state
 
         return <Container className="p-3">
             <div>
                 <Button onClick={this.backClick} icon={IconNames.ARROW_LEFT} text="Back" minimal />
             </div>
-            <FormGroup
-                label="File"
-                labelFor="file"
-            >
-                <FileInput id="file"
-                           fill
-                           text={file ? file.name : "Choose file..."}
-                           onInputChange={this.selectFile} />
-                {uploading && <ProgressBar value={this.state.progress} intent={Intent.PRIMARY} />}
 
-                <Button onClick={this.upload} disabled={!this.state.file} text="UPLOAD" />
-            </FormGroup>
+            <FileUploader handleChange={this.handleChange} multiple />
+            <Button onClick={this.upload} disabled={this.state.files.length === 0} text="UPLOAD" />
+
+            <div>
+                Total Size: {this.state.totalSize}
+            </div>
+
+            {uploading && <ProgressBar value={this.state.progress} intent={Intent.PRIMARY} />}
+            <Table>
+                <tbody>
+                {files.map(f => <tr key={f.file.name}>
+                    <td style={{width: "1%"}}><Button minimal icon={IconNames.TRASH} onClick={() => this.deleteFile(f)} /></td>
+                    <td>{f.file.name}</td>
+                    <td>{f.file.size}</td>
+                    <td>{uploading && <ProgressBar value={f.progress} intent={Intent.PRIMARY} />}</td>
+                </tr>)}
+                </tbody>
+            </Table>
         </Container>
+    }
+
+    deleteFile(f: File) {
+        const {files} = this.state
+        const i = files.indexOf(f)
+        files.splice(i, 1)
+        this.setState({files})
+    }
+
+    handleChange = (fileList: FileList) => {
+        const {files} = this.state
+        for (let i = 0; i < fileList.length; i++) {
+            files.push({
+                file: fileList[i],
+                progress: 0,
+                uploaded: 0
+            })
+        }
+
+        const totalSize = files.map(f => f.file.size).reduce((prev, next) => prev + next)
+
+        this.setState({files, totalSize})
     }
 
     backClick = () => {
@@ -44,29 +89,30 @@ export default class PatchUploadPage extends React.Component<{global?: GlobalSto
         this.props.history.push(`/projects/${project}/${tabid}`)
     }
 
-    selectFile = (event) => {
-        const file = event.target.files[0]
-        this.setState({
-            file: file,
-        });
-    }
-
     upload = () => {
         const project = this.props.global.project
-        const {file} = this.state
+        const {files} = this.state
 
         this.setState({
             uploading: true,
         });
 
-        api.project(project.code).uploadPatch(file, this.onProgress)
+        Promise.all(files.map(file => api.project(project.code).uploadPatch(file.file, (e) => this.onProgress(file, e))))
             .then(this.onUploaded)
             .finally(() => this.setState({uploading: false}))
     }
 
-    onProgress = (event) => {
+    onProgress = (file: UploadFile, event) => {
+        const {files, totalSize} = this.state
+
+        file.uploaded = event.loaded
+        file.progress = event.loaded / event.total
+
+        const totalUploaded = files.map(f => f.uploaded).reduce((prev, next) => prev + next)
+
         this.setState({
-            progress: event.loaded / event.total
+            progress: totalUploaded / totalSize,
+            files
         });
     }
 
